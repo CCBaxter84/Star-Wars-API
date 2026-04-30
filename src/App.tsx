c5f46b918738db28a2425ed0301787ab60179ed9
Christopher <troyceclark@gmail.com>
2026-04-30 05:26:53 -0600

    Added if block for loading state

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,46 @@ import type { Character } from './types/character'
 function App() {
   const [ characters, setCharacters ] = useState<Character[]>([])
   const [ count, setCount ] = useState(0)
+  const [ nextPage, setNextPage ] = useState<string | null>(null)
+  const [ prevPage, setPrevPage ] = useState<string | null>(null)
+  const [ isLoading, setIsLoading ] = useState(true)
 
   useEffect(() => {
+    setIsLoading(true)
     fetch('https://swapi.py4e.com/api/people?page=1')
       .then(res => res.json())
       .then(data => {
         setCharacters(data.results)
         setCount(data.count)
+        setNextPage(data.next)
+        setPrevPage(data.previous)
       })
       .catch(err => {
         console.error('Fetch failed:', err)
       })
+      .finally(() => setIsLoading(false))
   }, [])
-  return (
-    <div>
-      <h1>Star Wars API</h1>
-      <h3>Total Characters: {count}</h3>
+
+  if (isLoading) {
+    return (
       <section>
+        <h1>Star Wars API</h1>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <h1>Star Wars API</h1>
+      <article>
         {characters.map((character) => (
           <div key={character.name}>
             <h2>{character.name}</h2>
           </div>
         ))}
-      </section>
-    </div>
+      </article>
+    </section>
   )
 }
 
