d624b3b3a3c032f6b2e4ba023f21845d34c69038
Christopher <troyceclark@gmail.com>
2026-05-05 05:25:44 -0600

    ControlButton logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import ControlButtons from './components/ControlButtons'
 import Characters from './components/Characters'
@@ -9,11 +10,19 @@ function App() {
     "Planets",
     "Species"
   ]
+
+  const [activeButton, setActiveButton] = useState("Characters")
+
+  function handleButtonClick(button: string) {
+    setActiveButton(button)
+  }
+
   return (
     <main>
       <h1 className="title">Star Wars API</h1>
-      <ControlButtons buttons={buttons} />
-      <Characters />
+      <ControlButtons buttons={buttons} 
+                      onButtonClick={handleButtonClick}/>
+      {activeButton === "Characters" && <Characters />}
     </main>
   )
 }
--- src/components/ControlButtons.tsx
+++ src/components/ControlButtons.tsx
@@ -1,11 +1,20 @@
 import './ControlsButtons.css'
 
-function ControlButtons({ buttons }: { buttons: string[] }) {
+type Props = {
+  buttons: string[],
+  onButtonClick: (button: string) => void,
+}
+
+function ControlButtons({ buttons, onButtonClick }: Props) {
   return (
     <nav  style={{ marginBottom: '2rem' }}
           className="is-flex is-justify-content-space-between nav-buttons">
       {buttons.map((button) => (
-        <button key={button} className="button">{button}</button>
+        <button key={button} 
+                className="button" 
+                onClick={() => onButtonClick(button)}>
+          {button}
+        </button>
       ))}
     </nav>
   )
